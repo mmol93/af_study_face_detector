4b8fe860b019879d9d69745f78a87c2ce3d08aec
ljws93 <ljws93@naver.com>
2023-08-15 07:09:20 +0900

    compute face position

--- main.py
+++ main.py
@@ -22,16 +22,26 @@ while True:
 
     # detect face
     faces = detector(img)
-    face = faces[0]
+    # Check if any faces are detected
+    if len(faces) > 0:
+        face = faces[0]
 
     dlib_shape = predictor(img, face)
     shape_2d = np.array([[p.x, p.y] for p in dlib_shape.parts()])
 
+    # compute center of face
+    top_left = np.min(shape_2d, axis=0)
+    bottom_right = np.max(shape_2d, axis=0)
+
+    center_x, center_y = np.mean(shape_2d, axis=0).astype(int)
+
     # visualize
     img = cv2.rectangle(img, pt1=(face.left(), face.top(),), pt2=(face.right(), face.bottom()), color=(255, 255, 255), thickness=2, lineType=cv2.LINE_AA)
-
     for s in shape_2d:
-        cv2.circle(img, center=tuple(s), radius=1, color=(255,255,255), thickness=2, lineType=cv2.LINE_AA)
+        cv2.circle(img, center=tuple(s), radius=1, color=(255, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img, center=tuple(top_left), radius=1, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img, center=tuple(bottom_right), radius=1, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img, center=tuple((center_x, center_y)), radius=1, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
 
     # show movie
     cv2.imshow('img', img)
